65bbfdf66edce9a540db31eb54a5c85537c26e2a
mamejarra <mamejarra000@gmail.com>
2022-03-07 16:09:28 +0000

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 40,
                 ),
-                ElevatedButton(
+                /*ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     padding: EdgeInsets.all(10),
                     // ButtonStyle(backgroundColor: Colors.green[800]),
@@ -84,34 +84,153 @@ class _MyHomePageState extends State<MyHomePage> {
                         //backgroundColor: Colors.green,
                         fontSize: 20),
                   ),
-                ),
+                ),*/
                 SizedBox(
                   height: 50,
                 ),
-                Stack(
-                  children: [
-                    Card(
-                      elevation: 10,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                              child: Text(
-                            "Historique de paiements",
-                            textAlign: TextAlign.center,
-                          )),
-                          SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
+                SingleChildScrollView(
+                  child: Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Stack(
+                          children: [
+                            Card(
+                              elevation: 10,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "De sokhna Mbengue 77 121 33 23",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 15,
+                                      ),
+                                      Text(
+                                        "50.000F",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [Text("6 mars 2022 à 21: 37")],
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 15,
+                                      ),
+                                      Text(
+                                        "Retrait",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 240,
+                                      ),
+                                      Text(
+                                        "-5.000F",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [Text("8 janvier 2022 à 21: 37")],
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 15,
+                                      ),
+                                      Text(
+                                        "Dépot",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 240,
+                                      ),
+                                      Text(
+                                        "100.000F",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [Text("16 févier 2022 à 17: 52")],
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Transfert à bintou fall 77 567 00 23",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 15,
+                                      ),
+                                      Text(
+                                        "2.965F",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [Text("07 févier 2022 à 10: 40")],
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 15,
+                                      ),
+                                      Text(
+                                        "De Fallou 77 121 33 23",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 90,
+                                      ),
+                                      Text(
+                                        "12.000F",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text("14 décembre 2022 à 15: 59")
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                )
               ],
             ),
           ),
